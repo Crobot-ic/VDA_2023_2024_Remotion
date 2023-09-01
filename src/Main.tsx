ded0564ef901cdffc38dffcf7c37b194861124b7
Serquand <esteban.vincent@efrei.net>
2023-09-02 01:18:56 +0200

    Writting Sequences

--- src/Main.tsx
+++ src/Main.tsx
@@ -9,11 +9,84 @@ const Main: React.FC = () => {
 	return (
 		<>
 			<Series>
+				{/* Créé avec du code */}
 				<Series.Sequence 
 					durationInFrames={time`5s`}
 					name="Créé avec du code"
 				>	
 					<GeneratingMode />
+				</Series.Sequence>
+
+				{/* Accroche */}
+				<Series.Sequence 
+					durationInFrames={time`10s`}
+					name="Accroche"
+				>
+					<></>
+				</Series.Sequence>
+
+				{/* Roulette - Crobot'ic c'est */}
+				<Series.Sequence 
+					durationInFrames={time`5s`}
+					name="Roulette activités"
+				>
+					<></>
+				</Series.Sequence>
+
+				{/* Nos projets */}
+				<Series.Sequence 
+					durationInFrames={time`25s`}
+					name="Nos projets"
+				>
+					<></>
+				</Series.Sequence>
+
+				{/* D'autres projets */}
+				<Series.Sequence 
+					durationInFrames={time`5s`}
+					name="D'autres projets"
+				>
+					<></>
+				</Series.Sequence>
+
+				{/* Roulette - Partie 2 : Des formations */}
+				<Series.Sequence 
+					durationInFrames={time`5s`}
+					name="Des formations - Roulette"
+				>
+					<></>
+				</Series.Sequence>
+
+				{/* Des formations */}
+				<Series.Sequence 
+					durationInFrames={time`5s`}
+					name="Roulette activités"
+				>
+					<></>
+				</Series.Sequence>
+
+				{/* Roulette - Partie 3 : Des événements */}
+				<Series.Sequence 
+					durationInFrames={time`5s`}
+					name="Des événements - Roulette"
+				>
+					<></>
+				</Series.Sequence>
+
+				{/* Des événements */}
+				<Series.Sequence 
+					durationInFrames={time`10s`}
+					name="Des événements"
+				>
+					<></>
+				</Series.Sequence>
+
+				{/* Slide d'information */}
+				<Series.Sequence 
+					durationInFrames={time`5s`}
+					name="Informations"
+				>
+					<></>
 				</Series.Sequence>
 			</Series>
 		</>
